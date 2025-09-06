00d622a6e903de1020149e74926cbd44c9e0191d
Tirthes2004 <144465164+Tirthes2004@users.noreply.github.com>
2025-09-07 02:32:18 +0530

    Update urls.py

--- rainwater_harvesting/urls.py
+++ rainwater_harvesting/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for rainwater_harvesting project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# rainwater_harvesting/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,10 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('calculator.urls')),
-    path('', include('calculator.urls')),  # For root URL handling
-    path('', include('accounts.urls')),  
-    
+    path('api/v1/', include('calculator.urls')),  # Keep API endpoints
+    path('auth/', include('accounts.urls')),      # Put auth under /auth/ prefix
+    path('', include('calculator.urls')),         # Calculator handles root URLs
 ]
 
 if settings.DEBUG:
