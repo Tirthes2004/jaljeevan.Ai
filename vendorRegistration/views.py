c6098ab3788506b581d57a2a7fbe7140a419bcff
Tirthes2004 <144465164+Tirthes2004@users.noreply.github.com>
2025-10-15 20:55:16 +0530

    vendor page created

--- vendorRegistration/views.py
+++ vendorRegistration/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def vendorRegistration(request):
-    return render(request, 'vendorRegistration/vendorRegistration.html')
+    return render(request, 'vendorRegistration.html')
