b6d9e80347428bdd82e616275cc2a03e5f3ac23b
garaitapasi <tgarai168@gmail.com>
2025-10-15 20:54:53 +0530

    Vendor Registration

--- vendorRegistration/admin.py
+++ vendorRegistration/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Vendor
 
 # Register your models here.
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('shop_name', 'owner_name', 'phone_number', 'district')
+    search_fields = ('shop_name', 'owner_name', 'phone_number', 'PAN_Number', 'GSTIN_Number')
+    list_filter = ('district', 'service_type')
+    ordering = ('district',)
+    list_per_page = 20
+
+admin.site.register(Vendor, VendorAdmin)
