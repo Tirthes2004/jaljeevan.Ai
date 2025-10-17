c231aa958daabaccb913ca7c0d0340ef3b6d1a71
Tirthes2004 <144465164+Tirthes2004@users.noreply.github.com>
2025-10-18 00:53:20 +0530

    Revert "Merge branch 'main' of https://github.com/Tirthes2004/jaljeevan.Ai"
    This reverts commit 60b89f806607ee6ea6307c0169f4b3490d5d85e2, reversing
    changes made to a07a69549d99b0ed6f7dffc9f85c203a634af231.

--- vendorRegistration/urls.py
+++ vendorRegistration/urls.py
@@ -7,5 +7,4 @@ app_name = 'vendorRegistration'
 urlpatterns = [
     path('', views.vendorRegistration, name='vendorRegistration'),
     path('submit/', views.vendor_register, name='vendor_register'),
-    path('search/', views.search_vendors, name='search_vendors'),
 ]
