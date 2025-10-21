2aed32fc7b796a100bc6b68212480f0b1d7a3443
Tirthes2004 <144465164+Tirthes2004@users.noreply.github.com>
2025-10-21 11:29:35 +0530

    Update urls.py

--- GovtApplications/urls.py
+++ GovtApplications/urls.py
@@ -9,5 +9,6 @@ app_name = 'GovtApplications'
 urlpatterns = [
     path('', views.application_dashboard, name='application_dashboard'),
     path('form/',views.application_form, name='application_form'),
-
+    path('track/', views.track_applications, name='track_applications'),
+    path('api/submit/', views.submit_application, name='submit_application'),
 ]
